f15d76d670280e5768f544f3e08a988248077cdf
Brenden Smith <brenden@elektrafi.com>
2022-01-27 19:35:20 -0600

    My configure file

---
+++ configure.cmd
@@ -0,0 +1,1 @@
+./configure --enable-autodepend --with-{sound=yes,x-toolkit=gtk3,mailutils,pop,hesiod,wide-int,xpm,jpeg,tiff,gif,png,rsvg,lcms2,libsystemd,cairo,xml2,imagemagick,json,xft,harfbuzz,libotf,m17n-flt,toolkit-scroll-bars,xaw3d,xim,xdbe,dbus,gsettings,zlib,modules,threads,native-compilation,file-notification=yes,xwidgets,x,libgmp,included-regex} --without-{selinux}
